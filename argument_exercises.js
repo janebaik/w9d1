d8d076900fc9c9b9397383c27c12775ed2f92619
jane baik <janebaik@berkeley.edu>
2020-12-28 09:18:09 -0800

    sum

--- argument_exercises.js
+++ argument_exercises.js
@@ -1,1 +1,15 @@
-hello
+function sum(){
+    let sumFinal = 0;
+   console.log(arguments);
+   console.log(`${Object.keys(arguments)} line 4`);
+    const keysObject = Object.keys(arguments);
+    keysObject.forEach(function(ele) {
+        console.log(keysObject);
+        sumFinal += arguments[ele];
+        console.log(`${arguments[ele]} line 9`)
+    })
+    return sumFinal;
+}
+
+console.log(sum(1, 2, 3, 4) === 10);
+console.log(sum(1, 2, 3, 4, 5) === 15);
