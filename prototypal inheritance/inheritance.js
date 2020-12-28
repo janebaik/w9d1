77a1de656b21edffe47bd7ea6c9d631517f7bad1
jane baik <janebaik@berkeley.edu>
2020-12-28 13:58:19 -0800

    inheritance

---
+++ prototypal inheritance/inheritance.js
@@ -0,0 +1,28 @@
+
+
+// Surrogate.prototype = SuperClass.prototype;
+// Subclass.prototype = new Surrogate();
+// Subclass.prototype.constructor = Subclass;
+
+Function.prototype.inherits = function(superclass){
+    function Surrogate() { };
+    const fn = this;
+    Surrogate.prototype = superclass.prototype;
+    fn.prototype = new Surrogate();
+    fn.prototype.constructor = fn;
+}
+
+function MovingObject(){ 
+    function galaxy(){
+        console.log("galaxy");
+    }
+    
+}
+
+function Ship() { }
+Ship.inherits(MovingObject);
+Ship.galaxy();
+
+function Asteroid() { }
+Asteroid.inherits(MovingObject);
+
