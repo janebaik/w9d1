e0f5414736bcc733573280c839e025073a435891
ariel taylor <ariel.taylorr@gmail.com>
2020-12-28 11:06:34 -0800

    Workd on myBind

--- argument_exercises.js
+++ argument_exercises.js
@@ -1,15 +1,22 @@
 function sum(){
     let sumFinal = 0;
-   console.log(arguments);
-   console.log(`${Object.keys(arguments)} line 4`);
-    const keysObject = Object.keys(arguments);
-    keysObject.forEach(function(ele) {
-        console.log(keysObject);
-        sumFinal += arguments[ele];
-        console.log(`${arguments[ele]} line 9`)
+//    console.log(arguments);
+//    console.log(`${Object.keys(arguments)} line 4`);
+    const valueObject = Object.values(arguments);
+    valueObject.forEach(function(ele) {
+        sumFinal += ele;
+        // console.log(`${arguments[ele]} line 9`)
     })
     return sumFinal;
 }
 
-console.log(sum(1, 2, 3, 4) === 10);
-console.log(sum(1, 2, 3, 4, 5) === 15);
+// function sum(...args){
+//     let sumFinal = 0;
+//     for(let i = 0; i < args.length; i++){
+//         sumFinal += args[i];
+//     }
+//     return sumFinal
+// }
+
+// console.log(sum(1, 2, 3, 4) === 10);
+// console.log(sum(1, 2, 3, 4, 5) === 15);
